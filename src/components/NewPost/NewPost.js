75edd643a725559a9f85c480214bf23417642f62
Hobinx <binservmail@gmail.com>
2017-11-30 23:40:35 +0800

    set axios defauts baseURL

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -15,7 +15,7 @@ class NewPost extends Component {
       ...this.state
     };
 
-    axios.post('https://jsonplaceholder.typicode.com/posts', post).then(res => {
+    axios.post('/posts', post).then(res => {
       console.log(res);
     });
   };
